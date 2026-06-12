3cf68cadcc8c9f3b34f481339a340bb312cefba4
Bozhen JIANG <156669560+BozhenJIANG@users.noreply.github.com>
2026-06-12 15:49:51 +0800

    Update __init__.py

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -3,7 +3,7 @@ Training and evaluation utilities.
 """
 from .loss import compute_loss
 from .trainer import train_step_0, train_step_1_1, train_step_1_2, train_step_2
-from .evaluator import validate_model, evaluate_models, print_evaluation_results, run_power_flow_pypower
+from .evaluator import validate_model, print_evaluation_results, run_power_flow_pypower
 
 __all__ = [
     'compute_loss',
